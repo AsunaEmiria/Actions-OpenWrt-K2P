db6b200706ef34bf26cb738cfaa8f539804959d6
AsunaEmiria <49182516+AsunaEmiria@users.noreply.github.com>
2023-01-17 22:04:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-sed -i 's/dhcp/pppoe/g' package/base-files/files/lib/functions/uci-defaults.sh
