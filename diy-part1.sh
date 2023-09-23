0c80d97aa076b6b15e0a9a976785be1f32e9c994
AsunaEmiria <49182516+AsunaEmiria@users.noreply.github.com>
2023-09-24 02:57:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
